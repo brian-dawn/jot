8d7530cd6b85f7820bcc1c64d420a52edd24eb29
Brian Dawn <brian.t.dawn@gmail.com>
2020-03-28 13:21:40 -0500

    Added a couple commands, moved date format to rfc3339.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use notify_rust::Notification;
 
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use colorful::Color;
+use colorful::Colorful;
 use anyhow::Result;
 use chrono::prelude::*;
 use clap::{App, Arg, SubCommand};
+use regex::Regex;
+use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 // jot.txt contains a series of logs and stuff.
 // jot also needs to be a daemon process so it can parse jot files
@@ -19,17 +22,51 @@ use std::io::Write;
 //
 //
 // jot supports hashtags for topics #foo #bar and can display/edit tags.
+//
+
+#[derive(PartialEq, Eq, Clone)]
+struct JotLine {
+    datetime: DateTime<FixedOffset>,
+    raw: String,
+    message: String,
+    tags: Vec<String>,
+}
+
+impl JotLine {
+    fn pprint(&self) {
+        let pretty_date = self.datetime.format("%Y-%m-%d %H:%M").to_string().blue();
+        println!("[{}] {}", pretty_date, self.message)
+    }
+}
 
 /// Return a string for the date tag that is now.
 fn now() -> String {
-    let local: DateTime<Local> = Local::now();
+    let local: DateTime<Local> = Local::now().with_nanosecond(0).unwrap();
 
-    let date_str = local.format("%Y-%m-%d %H:%M %Z");
+    let date_str = local.to_rfc3339();
     format!("[{}]", date_str)
 }
 
-fn main() -> Result<()> {
+/// Parse a line in our jot log.
+fn parse_line(line: &str) -> Option<JotLine> {
+    let re = Regex::new(r"^\[(.*?)\] (.*)$").unwrap();
+    for caps in re.captures_iter(line) {
+        let date = caps.get(1)?.as_str().trim().to_owned();
+        let message = caps.get(2)?.as_str();
 
+        let parsed_date = DateTime::parse_from_rfc3339(&date).ok()?;
+        return Some(JotLine {
+            datetime: parsed_date,
+            raw: line.to_owned(),
+            message: message.to_string(),
+            tags: vec![]
+        })
+    }
+
+    None
+}
+
+fn main() -> Result<()> {
     let journal = "/Users/brian/Sync/journal.txt";
     let matches = App::new("jot")
         .version("0.1")
@@ -56,11 +93,17 @@ fn main() -> Result<()> {
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("cat") {
-
         let file = File::open(journal)?;
-        for line in io::BufReader::new(file).lines() {
 
-            println!("{}", line?);
+        for line in io::BufReader::new(file).lines() {
+            let ln = line?;
+            if let Some(parsed) = parse_line(&ln) {
+
+                parsed.pprint();
+            } else {
+                // print out just the raw string.
+                println!("{}", ln);
+            }
         }
         return Ok(());
     }
@@ -71,7 +114,6 @@ fn main() -> Result<()> {
     }
 
     if let Some(matches) = matches.subcommand_matches("down") {
-
         let local: DateTime<Local> = Local::now();
         let line = std::env::args()
             .skip_while(|arg| arg != "down") // Find the start of our messages.
