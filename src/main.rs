1e1a6f7270decbe80d803b2ee5348f9b4db94d6e
Brian Dawn <brian.t.dawn@gmail.com>
2020-04-04 18:40:41 -0500

    Add occurrences to tags.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[macro_use]
+extern crate prettytable;
+#[macro_use]
 extern crate lazy_static;
 
 use tempfile::NamedTempFile;
@@ -13,7 +15,7 @@ use clap::{App, Arg, SubCommand};
 use colorful::Colorful;
 use itertools::Itertools;
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -797,10 +799,93 @@ fn main() -> Result<()> {
 
     if let Some(_matches) = matches.subcommand_matches("tags") {
         // TODO: display how many jots have each tag.
-        let tags: HashSet<String> = stream_jots(config)?.flat_map(|jot| jot.tags).collect();
-        for tag in tags {
-            println!("{}", tag);
+        //let tags: HashSet<String> = stream_jots(config)?.flat_map(|jot| jot.tags).collect();
+        //for tag in tags {
+        //    println!("{}", tag);
+        //}
+
+        let mut all_tags = HashSet::new();
+        let mut notes = HashMap::new();
+        let mut remns = HashMap::new();
+        let mut todos = HashMap::new();
+        let increment = |map: &mut HashMap<String, usize>, key: &str| {
+            let insert = if let Some(val) = map.get(key) {
+                val + 1
+            } else {
+                1
+            };
+            map.insert(key.to_string(), insert);
+        };
+        for jot in stream_jots(config)? {
+            match jot.msg_type {
+                MessageType::Note => {
+                    for tag in jot.tags {
+                        increment(&mut notes, &tag);
+                        all_tags.insert(tag);
+                    }
+                }
+                MessageType::Reminder(_) => {
+                    for tag in jot.tags {
+                        increment(&mut remns, &tag);
+                        all_tags.insert(tag);
+                    }
+                }
+
+                MessageType::Todo(_) => {
+                    for tag in jot.tags {
+                        increment(&mut todos, &tag);
+                        all_tags.insert(tag);
+                    }
+                }
+            }
         }
+
+        use prettytable::{format, Cell, Row, Table};
+        let mut table = Table::new();
+        let format = format::FormatBuilder::new()
+            .separators(&[], format::LineSeparator::new('-', '+', '+', '+'))
+            .padding(0, 0)
+            .build();
+        table.set_format(format);
+        table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+        table.add_row(row![
+            "tag".bold(),
+            "notes".bold().blue(),
+            "todos".bold().magenta(),
+            "reminders".bold().yellow()
+        ]);
+        for tag in itertools::sorted(all_tags.into_iter()) {
+            let notes_cell = Cell::new_align(
+                &notes
+                    .get(&tag)
+                    .map(|s| s.to_string().blue())
+                    .unwrap_or("".to_string().bold())
+                    .to_string(),
+                format::Alignment::CENTER,
+            );
+
+            let todos_cell = Cell::new_align(
+                &todos
+                    .get(&tag)
+                    .map(|s| s.to_string().magenta())
+                    .unwrap_or("".to_string().bold())
+                    .to_string(),
+                format::Alignment::CENTER,
+            );
+
+            let remns_cell = Cell::new_align(
+                &remns
+                    .get(&tag)
+                    .map(|s| s.to_string().yellow())
+                    .unwrap_or("".to_string().bold())
+                    .to_string(),
+                format::Alignment::CENTER,
+            );
+
+            table.add_row(row![tag, notes_cell, todos_cell, remns_cell]);
+        }
+        table.printstd();
+
         return Ok(());
     }
 
