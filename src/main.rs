425e6340ea0959f18e28e0277d50ed83101226ba
Brian Dawn <brian.t.dawn@gmail.com>
2020-04-05 08:19:10 -0500

    Break out tags command into its own file.

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,12 @@ use clap::{App, Arg, SubCommand};
 use colorful::Colorful;
 use itertools::Itertools;
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use std::fs::OpenOptions;
 use std::io::Write;
 
+mod commands;
 mod config;
 mod constants;
 mod jot;
@@ -464,89 +465,7 @@ fn main() -> Result<()> {
     }
 
     if let Some(_matches) = matches.subcommand_matches("tags") {
-        let mut all_tags = HashSet::new();
-        let mut notes = HashMap::new();
-        let mut remns = HashMap::new();
-        let mut todos = HashMap::new();
-        let increment = |map: &mut HashMap<String, usize>, key: &str| {
-            let insert = if let Some(val) = map.get(key) {
-                val + 1
-            } else {
-                1
-            };
-            map.insert(key.to_string(), insert);
-        };
-        for jot in stream_jots(config)? {
-            match jot.msg_type {
-                MessageType::Note => {
-                    for tag in jot.tags {
-                        increment(&mut notes, &tag);
-                        all_tags.insert(tag);
-                    }
-                }
-                MessageType::Reminder(_) => {
-                    for tag in jot.tags {
-                        increment(&mut remns, &tag);
-                        all_tags.insert(tag);
-                    }
-                }
-
-                MessageType::Todo(_) => {
-                    for tag in jot.tags {
-                        increment(&mut todos, &tag);
-                        all_tags.insert(tag);
-                    }
-                }
-            }
-        }
-
-        use prettytable::{format, Cell, Table};
-        let mut table = Table::new();
-        let format = format::FormatBuilder::new()
-            .separators(&[], format::LineSeparator::new('-', '+', '+', '+'))
-            .padding(0, 0)
-            .build();
-        table.set_format(format);
-        table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
-        table.add_row(row![
-            "tag".bold(),
-            "notes".bold().blue(),
-            "todos".bold().magenta(),
-            "reminders".bold().yellow()
-        ]);
-        for tag in itertools::sorted(all_tags.into_iter()) {
-            let notes_cell = Cell::new_align(
-                &notes
-                    .get(&tag)
-                    .map(|s| s.to_string().blue())
-                    .unwrap_or("".to_string().bold())
-                    .to_string(),
-                format::Alignment::CENTER,
-            );
-
-            let todos_cell = Cell::new_align(
-                &todos
-                    .get(&tag)
-                    .map(|s| s.to_string().magenta())
-                    .unwrap_or("".to_string().bold())
-                    .to_string(),
-                format::Alignment::CENTER,
-            );
-
-            let remns_cell = Cell::new_align(
-                &remns
-                    .get(&tag)
-                    .map(|s| s.to_string().yellow())
-                    .unwrap_or("".to_string().bold())
-                    .to_string(),
-                format::Alignment::CENTER,
-            );
-
-            table.add_row(row![tag, notes_cell, todos_cell, remns_cell]);
-        }
-        table.printstd();
-
-        return Ok(());
+        return commands::tags::tags_command(config);
     }
 
     // matches.print_help();
